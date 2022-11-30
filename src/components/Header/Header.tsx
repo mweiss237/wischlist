eb96b1fd53a0ed27f149b7d34c65a5a54091da86
Moritz <moritz.weiss@sevencooks.com>
2022-11-30 10:26:37 +0100

    fix some styling problems

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "./Header.module.css";
 import favicon from "../../../public/favicon.ico";
+import Link from "next/link";
 
 type HeaderParams = {};
 
@@ -12,13 +13,15 @@ const Header = ({}: HeaderParams) => {
         <title>Wischlist</title>
       </Head>
       <div className="crit_header">
-        <Image
-          className="crit_header_logo"
-          src={favicon}
-          alt="vercel icon"
-          width={40}
-          height={40}
-        />
+        <Link href="/">
+          <Image
+            className="crit_header_logo"
+            src={favicon}
+            alt="vercel icon"
+            width={40}
+            height={40}
+          />
+        </Link>
 
         <h1 className={`crit_header_title ${styles.headline}`}>Wischlist</h1>
       </div>
