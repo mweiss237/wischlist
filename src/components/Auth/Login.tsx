9f5e65608ed69416095a784d3279ccef4f488bc9
Moritz <moritz.weiss@sevencooks.com>
2022-12-14 19:36:31 +0100

    added login component draft

---
+++ src/components/Auth/Login.tsx
@@ -0,0 +1,63 @@
+"use client";
+import crypto from "crypto";
+import React, { FormEvent } from "react";
+import styles from "./Login.module.scss";
+
+const Login = () => {
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+
+  const authorize = async (e: FormEvent) => {
+    e.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    if (!email || !password) return;
+
+    const hash = crypto.createHash("md5").update(password).digest("hex");
+
+    /* --- only with firebase-admin change which breaks currently ---*/
+    // if ((await userDB.where("email", "==", email)).length > 0) {
+    //   alert(`Email ${email} bereits registriert.`);
+    //   return;
+    // }
+
+    // await userDB
+    //   .add({
+    //     email: emailRef.current.value,
+    //     passwordHash: hash,
+    //     lastLogin: new Date(),
+    //   })
+    // .then((result) => alert(`id ${result.id} added`));
+  };
+
+  return (
+    <form onSubmit={authorize} className={styles.wrapper}>
+      <label className={styles.inputlabel} htmlFor="#email">
+        Email
+      </label>
+      <input
+        id="email"
+        type={"email"}
+        className={styles.textfield}
+        ref={emailRef}
+        placeholder="Email"
+      />
+      <label className={styles.inputlabel} htmlFor="#password">
+        Passwort
+      </label>
+      <input
+        id="password"
+        type={"password"}
+        className={styles.textfield}
+        ref={passwordRef}
+        placeholder="Passwort"
+      />
+      <span className={styles.buttonWrapper}>
+        <button type={"submit"}>Registrieren</button>
+        <button type={"reset"}>Zurücksetzen</button>
+      </span>
+    </form>
+  );
+};
+
+export default Login;
