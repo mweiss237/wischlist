b1b47233ea8d00e622a93fcb456c07da98765878
Moritz Weiß <moritz.weiss95@hotmail.de>
2024-05-08 12:36:25 +0200

    add speed insight + bump 0.1.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "styles/globals.scss"
 import "styles/critical.scss"
 import { AuthProvider } from "lib/auth"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -23,6 +24,7 @@ export default function RootLayout({
           {children}
         </AuthProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
