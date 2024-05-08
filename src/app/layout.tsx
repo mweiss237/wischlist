c52eb4072de98bab5e905b6fc25a72eb23bc8d6e
Moritz Weiß <moritz.weiss95@hotmail.de>
2024-05-08 12:41:52 +0200

    remove analytics again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "styles/globals.scss"
 import "styles/critical.scss"
 import { AuthProvider } from "lib/auth"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -23,8 +21,6 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
