c2acd3b361834e3a9c7dcfbc32e89fb3288e2187
Moritz Weiß <moritz.weiss95@hotmail.de>
2024-07-16 23:09:43 +0200

    add share api for supporting browsers

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -10,6 +10,7 @@ import { useList } from "lib/list"
 
 
 import Link from "next/link"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 import React from "react"
 import { useState, useCallback } from "react"
@@ -33,13 +34,19 @@ const List = ({ params }: { params: { listId: string } }) => {
       .some(entry => entry.taken?.timestamp !== undefined)
     , [entries])
 
-  const donors = React.useMemo(() =>
+  const takenEntries = React.useMemo(() =>
     Object.values(entries || {})
-      .filter(entry => entry.taken !== undefined && entry.taken.giver)
-      .map(entry => entry.taken?.giver)
+      .filter(entry => entry.taken !== undefined && entry.taken.timestamp)
     , [entries])
 
   const [isClicked, setClicked] = useState(false)
+  let canShare = false
+  try {
+    canShare = navigator.canShare()
+  } catch (_) {
+    console.log("WebShare api not available")
+  }
+
 
   const handleDeleteList = useCallback(() => {
     if (confirm("Möchtest du diese Liste wirklich unwiederbringlich löschen?")) {
@@ -60,9 +67,19 @@ const List = ({ params }: { params: { listId: string } }) => {
     updateListTitle(listName)
   }
 
-  const copyUrlToClipboard = () => {
+  const shareOrCopyUrlToClipboard = () => {
     setClicked(true)
-    navigator.clipboard.writeText(`${window.location.href}/share`)
+
+    canShare ? navigator.share({
+      title: `Wischlist - ${list?.title} von ${user?.displayName}`,
+      text: `Hey, ich möchte diese Wunschliste mit dir teilen!`,
+      url: `${window.location.href}/share`
+    }) : navigator.clipboard.writeText(`${window.location.href}/share`)
+
+
+
+
+    setTimeout(() => setClicked(false), 2000)
   }
 
 
@@ -70,21 +87,11 @@ const List = ({ params }: { params: { listId: string } }) => {
   return (
     <>
       {alreadyPickedSome ?
-        <div className={indieFlowerFont.className}>
-          <div className={`${styles.pickedInfo} crit_centered`}>
-            <PresentSVG color="#ffc107" />
-            <p >
-              Jemand schenkt etwas aus der Liste!
-            </p>
-          </div>
-          {donors.length > 0 ? <div className="crit_centered crit_row">
-            <p>Aktuelle Schenker:</p>
-            <ul>
-              {donors.map((donor, i) => <li key={`donorEntry_${i}`}>{donor}</li>)}
-            </ul>
-          </div> : null
-          }
-
+        <div className={`${indieFlowerFont.className} ${styles.pickedInfo} crit_centered`}>
+          <PresentSVG color="#ffc107" />
+          <p>
+            Bereits {takenEntries.length} Wünsche reserviert!
+          </p>
         </div>
         : null}
       <DeleteTrashCan onDelete={handleDeleteList} />
@@ -93,42 +100,58 @@ const List = ({ params }: { params: { listId: string } }) => {
         <span>Liste:</span>
         <input onClick={handleChangeListName} type="text" readOnly value={list?.title} className={`${indieFlowerFont.className} crit_textinput`} />
       </div>
-      {user ? (
-        <>
-          <div className={styles.shareWrapper}>
-            <button className={`crit_button ${styles.share} ${isClicked && styles.clicked}`} onClick={copyUrlToClipboard}>Link {!isClicked ? "kopieren" : "kopiert!"}</button>
-          </div>
-          <div className={styles.list}>
+      {
+        user ? (
+          <>
+            <div className={styles.shareWrapper}>
 
-            {entries && Object.keys(entries).map((entryId) => {
-              const entry = entries[entryId]
-              return (
-                <Card
-                  key={`wish_${entryId}`}
-                  id={entryId}
-                  value={entry.text}
-                  link={entry.link}
-                  priority={entry.priority}
-                  onDelete={() => removeEntry(entryId)}
-                  onSave={(_id, value) => { updateEntry(entryId, { text: value }) }}
-                  onAddLink={(value) => { updateEntry(entryId, { link: value }) }}
-                  onSetPriority={(priority) => { updateEntry(entryId, { priority }) }}
+              <input type="text" readOnly value={`${window.location.href}/share`} />
+              <button
+                title={canShare ? "Liste teilen" : "Link kopieren"}
+                className={`crit_button ${styles.share} ${isClicked && styles.clicked}`}
+                onClick={shareOrCopyUrlToClipboard}>
+                <Image
+                  src={canShare ? "/link.svg" : "/copy.svg"}
+                  alt={canShare ? "Liste teilen" : "Link kopieren"}
+                  height={20}
+                  width={20}
+                  unoptimized
+                  loading="lazy"
                 />
-              )
-            })}
-            <AddCard callback={() => addEntry({
-              text: "",
-            })} />
-          </div>
+              </button>
+            </div>
+            <div className={styles.list}>
+
+              {entries && Object.keys(entries).map((entryId) => {
+                const entry = entries[entryId]
+                return (
+                  <Card
+                    key={`wish_${entryId}`}
+                    id={entryId}
+                    value={entry.text}
+                    link={entry.link}
+                    priority={entry.priority}
+                    onDelete={() => removeEntry(entryId)}
+                    onSave={(_id, value) => { updateEntry(entryId, { text: value }) }}
+                    onAddLink={(value) => { updateEntry(entryId, { link: value }) }}
+                    onSetPriority={(priority) => { updateEntry(entryId, { priority }) }}
+                  />
+                )
+              })}
+              <AddCard callback={() => addEntry({
+                text: "",
+              })} />
+            </div>
 
 
-        </>
-      ) : (
-        <p>
-          <Link href="/login">Logge dich ein</Link>, um deine Liste sehen zu
-          können.
-        </p>
-      )}
+          </>
+        ) : (
+          <p>
+            <Link href="/login">Logge dich ein</Link>, um deine Liste sehen zu
+            können.
+          </p>
+        )
+      }
     </>
   )
 }
